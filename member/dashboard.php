c41e3a02aac258608c9b35edf47e996b510c8f2d
Ajmain-Fayek <ajmainfayek733@gmail.com>
2025-09-12 20:24:21 +0600

    member-view dahsboard template complete

--- member/dashboard.php
+++ member/dashboard.php
@@ -1,3 +1,19 @@
+<?php
+
+// Example member data (dynamic later from DB)
+$member = [
+  "name" => "Ajmain",
+  "meals" => 15,
+  "deposit" => 2025,
+  "lastDeposit" => 100,
+  "lastDepositDateTime" => "2025-09-10",
+  "balance" => 1234,
+  "charge" => 1234,
+  "mealRate" => 35.4
+];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,8 +38,48 @@
     ?>
 
     <!-- Main container -->
-    <div class="p-4">
-      jsdffjhskjgfskg
+    <div class="p-4 space-y-4 w-full bg-radial-[at_25%_25%] from-purple-100 to-blue-200">
+      <div class="bg-white shadow-md rounded-2xl overflow-hidden border border-gray-200">
+        <!-- Header -->
+        <div class="px-4 py-3 bg-amber-500">
+          <h2 class=" text-lg font-semibold text-center"><?= $member["name"] ?></h2>
+        </div>
+
+        <!-- Body -->
+        <div class="p-4 space-y-2 text-center">
+          <div class="flex justify-between">
+            <span class="font-medium text-gray-600">Total Meal</span>
+            <span class="font-bold"><?= $member["meals"] ?></span>
+          </div>
+          <div class="flex justify-between">
+            <span class="font-medium text-gray-600">Totoal Deposit</span>
+            <span class="font-bold"><?= number_format($member["deposit"], 2) ?></span>
+          </div>
+          <div class="flex justify-between">
+            <span class="font-medium text-gray-600">Last Deposit</span>
+            <span class="font-bold"><?= number_format($member["lastDeposit"], 2) ?></span>
+          </div>
+          <div class="flex justify-between">
+            <span class="font-medium text-gray-600">Last Deposit Date</span>
+            <span class="font-bold"> <?= date('d M Y', strtotime($member["lastDepositDateTime"])) ?></span>
+          </div>
+          <div class="flex justify-between">
+            <span class="font-medium text-gray-600">Meal Rate</span>
+            <span class="font-bold text-blue-600"><?= number_format($member["mealRate"], 2) ?></span>
+          </div>
+          <div class="flex justify-between">
+            <span class="font-medium text-gray-600">Cost</span>
+            <span class="font-bold text-red-600"><?= number_format($member["charge"], 2) ?></span>
+          </div>
+          <div class="flex justify-between">
+            <span class="font-medium text-gray-600">Balance</span>
+            <span class="font-bold 
+              <?= $member["balance"] >= 0 ? 'text-green-600' : 'text-red-600' ?>">
+              <?= number_format($member["balance"], 2) ?>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
 
   </div>
